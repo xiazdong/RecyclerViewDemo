7054731c7601b4f6b3af0dc7f22a274cdead28e9
xiazdong <xiazdong@126.com>
2016-10-12 09:08:07 +0800

    Demo7：Adapter万能适配器

---
+++ app/src/main/java/me/xiazdong/recyclerviewdemo/demo7/QuickAdapter.java
@@ -0,0 +1,65 @@
+package me.xiazdong.recyclerviewdemo.demo7;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by xiazdong on 16/10/12.
+ */
+public abstract class QuickAdapter<T> extends RecyclerView.Adapter<QuickAdapter.VH>{
+
+    private List<T> mDatas;
+
+    public QuickAdapter(List<T> datas){
+        this.mDatas = datas;
+    }
+
+    public abstract int getLayoutId(int viewType);
+
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        return VH.get(parent,getLayoutId(viewType));
+    }
+
+    @Override
+    public void onBindViewHolder(VH holder, int position) {
+        convert(holder, mDatas.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public abstract void convert(VH holder, T data, int position);
+
+    static class VH extends RecyclerView.ViewHolder{
+        private SparseArray<View> mViews;
+        private View mConvertView;
+
+        private VH(View v){
+            super(v);
+            mConvertView = v;
+            mViews = new SparseArray<>();
+        }
+
+        public static VH get(ViewGroup parent, int layoutId){
+            View convertView = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+            return new VH(convertView);
+        }
+
+        public <T extends View> T getView(int id){
+            View v = mViews.get(id);
+            if(v == null){
+                v = mConvertView.findViewById(id);
+                mViews.put(id, v);
+            }
+            return (T)v;
+        }
+    }
+}
